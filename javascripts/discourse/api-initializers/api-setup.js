5368b9394fe01e22eac83f5747cac00ba6dc41ea
bluefroguk1 <62796282+bluefroguk1@users.noreply.github.com>
2024-12-07 00:31:58 +0000

    112

--- javascripts/discourse/api-initializers/api-setup.js
+++ javascripts/discourse/api-initializers/api-setup.js
@@ -99,7 +99,7 @@ export default apiInitializer("0.11.1", (api) => {
   api.addComposerToolbarPopupMenuOption({
     action: (toolbarEvent) => {
       const text = "Text";
-      toolbarEvent.applySurround('<mark>', '</mark>', text, { noTranslate: true });
+      toolbarEvent.applySurround('<mark>', '</mark>', text);
     },
     icon: "highlighter",
     label: "highlight_button_title",
